dbf0495e5e8cf503811cc445c38b4dc4a922e1bd
Joe Schr <joe.vandelay@gmail.com>
2019-10-21 13:51:30 +0000

    wip, adds postgraphile-module, 1:1 copy from Atinux repo

--- backend/src/nuxtjs/nuxt.config.ts
+++ backend/src/nuxtjs/nuxt.config.ts
@@ -5,18 +5,18 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "graphile starter using nuxt.js w/ TS" }
+      {
+        hid: "description",
+        name: "description",
+        content: "graphile starter using nuxt.js w/ TS",
+      },
     ],
-    link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   loading: { color: "#3B8070" },
   css: ["~/assets/css/main.css"],
   build: {},
   buildModules: ["@nuxt/typescript-build"],
-  modules: [
-    "@nuxtjs/axios",
-  ],
-  axios: {}
-}
+  modules: ["@nuxtjs/apollo", "~/modules/nuxt-postgraphile", "@nuxtjs/axios"],
+  axios: {},
+};
