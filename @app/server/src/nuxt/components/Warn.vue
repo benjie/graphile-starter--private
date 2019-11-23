439310c3f89eddec283861125ec79049bd170be1
Joe Schr <joe.vandelay@gmail.com>
2019-11-23 00:57:40 +0000

    Add `Warn` component

---
+++ @app/server/src/nuxt/components/Warn.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <span v-if="state.okay"> <slot></slot> </span>
+    <span v-else>
+      <Badge :dot="true">
+        <slot></slot>
+      </Badge>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Badge } from "ant-design-vue";
+import { createComponent, reactive } from "@vue/composition-api";
+
+export default createComponent({
+  name: "DefaultLayout",
+  components: {
+    Badge,
+  },
+  props: { okay: Boolean },
+  setup(props) {
+    const state = reactive({
+      okay: props.okay,
+    });
+
+    return { state };
+  },
+});
+</script>
+
+<style lang="less" scoped></style>
